86ab0bd3c80e1be21bc61cc9278a849bf291cff4
krsnthmm <kriscillay@gmail.com>
2023-08-14 12:08:57 +0800

    made a bare-bones settings menu

---
+++ DX1224 Team 10/Assets/PlayerPrefsManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerPrefsManager
+{
+    public static void Save(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
+    }
+
+    public static float Load(string key)
+    {
+        return PlayerPrefs.GetFloat(key);
+    }
+}
