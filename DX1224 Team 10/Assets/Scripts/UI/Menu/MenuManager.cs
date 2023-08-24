8010a954f506a81f4e25293c71ca48f23a8b63fe
krsnthmm <kriscillay@gmail.com>
2023-08-24 14:42:31 +0800

    updated menu and pause to remove save/load data things
    saving and loading isn't a feature of our proj anymore soo

--- DX1224 Team 10/Assets/Scripts/UI/Menu/MenuManager.cs
+++ DX1224 Team 10/Assets/Scripts/UI/Menu/MenuManager.cs
@@ -8,24 +8,12 @@ public class MenuManager : MonoBehaviour
     public GameObject settingsMenuUI;
     public GameObject mainMenuUI;
 
-    public void NewGame()
+    public void PlayGame()
     {
-        // Create new save data?
         // Open game with new player data
         sceneLoader.LoadScene("LoadingScreen");
         playerData.SetDefaults();
         shop.SetDefaults();
-
-        // set timeScale to 1
-        // this is so that the scene to load isn't paused after returning to the main menu from the pause menu
-        Time.timeScale = 1f;
-    }
-
-    public void LoadGame()
-    {
-        // Load previous player data
-        // Load game scene
-        sceneLoader.LoadScene("LoadingScreen");
 
         // set timeScale to 1
         // this is so that the scene to load isn't paused after returning to the main menu from the pause menu
