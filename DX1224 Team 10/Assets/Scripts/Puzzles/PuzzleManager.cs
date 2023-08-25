133a3ada6e0250fcb3ecb799193a172df80859d9
krsnthmm <kriscillay@gmail.com>
2023-08-25 09:27:53 +0800

    fixes
    - colliders
    - fixed box puzzle constantly playing audio after pushing a box into the hole

--- DX1224 Team 10/Assets/Scripts/Puzzles/PuzzleManager.cs
+++ DX1224 Team 10/Assets/Scripts/Puzzles/PuzzleManager.cs
@@ -5,23 +5,21 @@ public class PuzzleManager : MonoBehaviour
     [SerializeField] private BoxPuzzle[] boxPuzzles;
     [SerializeField] private AudioPlayer audioPlayer;
     public bool boxPuzzleCompleted = false;
-    private int boxCount = 0;
 
     // Update is called once per frame
     void Update()
     {
+        // assume boxPuzzleCompleted is true
+        boxPuzzleCompleted = true;
+
         // check if each box in the array is in its respective hole
         for (int i = 0; i < boxPuzzles.Length; i++)
         {
-            if (boxPuzzles[i].isBoxInHole)
+            if (!boxPuzzles[i].isBoxInHole)
             {
-                if (boxCount >= boxPuzzles.Length)
-                {
-                    boxPuzzleCompleted = true;
-                    audioPlayer.PlayClip(3);
-                }
+                // if any box(es) are not in their respective holes, boxPuzzleCompleted is false
+                boxPuzzleCompleted = false;
             }
-            boxCount++;
         }
     }
 }
